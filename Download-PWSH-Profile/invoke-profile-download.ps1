aa143e86ee6b5b6e247da6c45ce333906e2040ed
Niklas Rast <47035125+niklasrast@users.noreply.github.com>
2023-07-21 20:58:00 +0200

    Update invoke-profile-download.ps1

--- Download-PWSH-Profile/invoke-profile-download.ps1
+++ Download-PWSH-Profile/invoke-profile-download.ps1
@@ -1,1 +1,1 @@
-Invoke-WebRequest -Uri "https://github.com/niklasrast/pwsh-profile/raw/main/Microsoft.PowerShell_profile.ps1" -OutFile $PROFILE
+Invoke-WebRequest -Uri "https://github.com/niklasrast/pwsh-profile/raw/main/Microsoft.PowerShell_profile.ps1" -OutFile $PROFILE -Force
